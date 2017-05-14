37fcf575def25a848b2c10fb8f2893a292517ab9
Moein <Moein>
2017-05-14 18:02:22 +0430

    database download and upload button

--- dist/js/app/database.js
+++ dist/js/app/database.js
@@ -1,2 +1,21 @@
 angular.module("assetAdminPanel").controller('databaseCtrl', function($scope,$http){
+
+  var begir = this ;
+
+  this.download = function(){
+
+    $scope.serverUrl = "https://api.gandom.co/api/api/v1" ;
+    $scope.header = {'Access-Token':'User Request-Access token given after login'} ;
+
+    $http.get($scope.serverUrl + "/database" + {headers: $scope.header})
+  }
+
+  this.upload = function(){
+
+    $scope.serverUrl = "https://api.gandom.co/api/api/v1" ;
+    $scope.header = {'Access-Token':'User Request-Access token given after login'} ;
+
+    $http.post($scope.serverUrl + "/database" + {headers: $scope.header}) ;
+
+  }
 });
