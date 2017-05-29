4ad9a1e53cd2e946aa13c2fb4c06b04526e351f7
hamed17n <hamed.kabiri@gandom.co>
2017-05-29 15:50:27 +0430

    change pass for user is ok

--- dist/js/app/changePassword.js
+++ dist/js/app/changePassword.js
@@ -1,36 +1,21 @@
-angular.module("assetAdminPanel").controller('changepassCtrl', function($scope,$http,mainAsset,$cookieStore){
+angular.module("assetAdminPanel").controller('changepassCtrl',
+  function($scope, $http, mainAsset, $cookieStore, requestHelper){
 
-    var begir = this;
-    $scope.assetData = $cookieStore.get("assetData")
+    var controller = this;
+
+    $scope.assetData = $cookieStore.get("assetData");
     $scope.serverUrl = mainAsset.getUrl();
-    $scope.header = {'Content-Type': 'application/json' , 'Access-Token': $scope.assetData.access_token} ;
+
+    this.changePassUrl = $scope.serverUrl + 'user/' + $scope.assetData.id + '/password';
+
+    this.changePassword = function(){
+      requestHelper.put(controller.changePassUrl, controller.obj, $scope, function() {
+
+        controller.obj = {};
+        controller.tmp = {};
+      },true);
+    }
 
 
-      this.check=function(){
-      if(begir.newPass === begir.newPassAgain)
-      {
-        return false;
-      }
-      else
-        {
-          return true;
-        }
-
-      };
-
-    this.sendPassword = function(){
-    var userPasswordData = {}
-    userPasswordData.old_password = begir.oldPass;
-    userPasswordData.new_password = begir.newPass;
-    $http.put($scope.serverUrl + "user/" + $scope.assetData.id + "/password", userPasswordData , {headers: $scope.header})
-    .then(function successCallback(response)
-    {
-
-    },
-    function errorCallback(response)
-    {
-
-    });
-  }
 
 });
