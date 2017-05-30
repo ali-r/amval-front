1bc5252c6153a64f080d6672b6fc059e9a7c79ee
hamed17n <hamed.kabiri@gandom.co>
2017-05-30 18:22:45 +0430

    database finish

--- dist/js/app/database.js
+++ dist/js/app/database.js
@@ -1,5 +1,5 @@
 angular.module("assetAdminPanel").controller('databaseCtrl',
-  function($scope, $http, $cookieStore, mainAsset, requestHelper){
+  function($scope, $http, $cookieStore, mainAsset, requestHelper, $window){
 
     var controller = this;
     $scope.assetData = $cookieStore.get("assetData");
@@ -8,16 +8,27 @@ angular.module("assetAdminPanel").controller('databaseCtrl',
     this.databaseUrl = $scope.serverUrl + 'database';
 
     this.download = function(){
-      requestHelper.get(controller.databaseUrl + '/backup', $scope, function(data) {
-        console.log(data);
+      requestHelper.post(controller.databaseUrl + '/backup', {}, $scope, function(response) {
+        requestHelper.get(response.data.download_url, $scope, function(res) {
+          /*uriContent = "data:application/octet-stream;charset=utf-8," + encodeURIComponent(res.data);
+          $window.open(uriContent,'file.txt');*/
+          var str = res.data;
+          var uri = 'data:text/csv;charset=utf-8,' + str;
+          var d = new Date();
+          var downloadLink = document.createElement("a");
+          downloadLink.href = uri;
+          downloadLink.download = "database-" + d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate()
+          + '_' + d.getHours() + ':' + d.getMinutes() + ".asset";
+          document.body.appendChild(downloadLink);
+          downloadLink.click();
+          document.body.removeChild(downloadLink);
+        },true)
       },true)
     }
 
     this.upload = function(){
+      requestHelper.put(controller.databaseUrl, {'database' : controller.file}, $scope, function() {
 
-      $scope.header = {'Access-Token':'User Request-Access token given after login'} ;
-
-      $http.post($scope.serverUrl + "/database" + {headers: $scope.header}) ;
-
+      },true);
     }
 });
