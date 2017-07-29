3706ee7899db2daad22eaeb8ef5c195186208e19
eslahi <09395621998@elenoon.ir>
2017-07-29 20:32:05 +0430

    select and add multiple products

--- dist/js/app/controllers/invoice.js
+++ dist/js/app/controllers/invoice.js
@@ -133,16 +133,28 @@ angular.module("assetAdminPanel").controller('invoiceCtrl',
       };
 
       controller.pushProduct = function(item){
-        if(controller.obj.products === undefined){controller.obj.products = []};
-        controller.obj.products.push(item)
-        controller.obj.num_of_products = controller.obj.products.length;
-        controller.closeModal('select');
+        if(controller.tmp.selectedProducts === undefined){controller.tmp.selectedProducts = []};
+        controller.tmp.selectedProducts.push(item);
+      }
+
+      controller.deselect = function(item_index){
+        controller.tmp.selectedProducts.remove(item_index);
       }
 
       controller.deleteProduct = function(item_index){
         controller.obj.products.remove(item_index);
         controller.obj.num_of_products = controller.obj.products.length;
         controller.closeModal('delete');
+      }
+
+      controller.pushSelectedProducts = function(){
+        if(controller.obj.products === undefined){controller.obj.products = []};
+        controller.tmp.selectedProducts.forEach(function(item,index){
+          controller.obj.products.push(item);
+        });
+        controller.obj.num_of_products = controller.obj.products.length;
+        controller.tmp.selectedProducts = [];
+        controller.closeModal('select');
       }
 
       this.uploadPic = function() {
