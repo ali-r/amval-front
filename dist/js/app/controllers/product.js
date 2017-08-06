6772a12a5eeb73348ab399ee44736a87615fd821
hamed17n <hamed.kabiri@gandom.co>
2017-08-06 23:55:03 +0430

    bundle bug fix

--- dist/js/app/controllers/product.js
+++ dist/js/app/controllers/product.js
@@ -100,9 +100,6 @@ angular.module("assetAdminPanel").controller('productCtrl',
       sendCopyObj.is_bundle = false;
       delete sendCopyObj.children;
     }
-    else{
-      sendCopyObj.children = [];
-    }
 
     sendCopyObj.deprication_type = Number(sendCopyObj.deprication_type);
     delete sendCopyObj.price;
