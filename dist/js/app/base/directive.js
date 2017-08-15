7e3b76ae93447751c51c5bcdea6a8dcee962e5dc
eslahi <09395621998@elenoon.ir>
2017-08-15 17:43:52 +0430

    [AM-234] pagination fixed

--- dist/js/app/base/directive.js
+++ dist/js/app/base/directive.js
@@ -62,13 +62,13 @@ app.directive('reqPagination', function() {
             };
             break;
           case 'first_mid':
-            if(pageMeta.pages<5 && pageMeta.prev && pageMeta.next) return true;
-            else if(pageMeta.page>2) return false;
+            var prevPage = pageMeta.page - 1;
+            if(prevPage - 1 > 1) return false;
             else return true;
             break;
           case 'end_mid':
-            if(pageMeta.pages<5 && pageMeta.prev && pageMeta.next) return true;
-            else if(pageMeta.pages > 1 && pageMeta.page < pageMeta.pages -1) return false;
+            var nextPage = pageMeta.page + 1;
+            if(pageMeta.pages - nextPage > 1) return false;
             else return true;
             break;
         };
