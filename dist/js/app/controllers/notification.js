37ae3c39174a26e6b8bb3bb0d5daa84ae7c18540
eslahi <09395621998@elenoon.ir>
2017-10-02 13:46:36 +0330

    loading and converting notifs in notification page

--- dist/js/app/controllers/notification.js
+++ dist/js/app/controllers/notification.js
@@ -1,19 +1,45 @@
 angular.module("assetAdminPanel").controller('notificationCtrl',
 function($scope, mainAsset, requestHelper, crud, $routeParams) {
 
-var controller = this;
-var apiName = 'notification';
+    var controller = this;
+    var apiName = 'notification';
+    var scope = $scope;
+    $scope.page = 1;
 
-$scope.uploadUrl = mainAsset.getUploadUrl();
-
-controller.searchObject = [
-];
-
-$scope.page = 1;
-
-$scope.apiUrl = mainAsset.getUrl() + apiName;
-
-crud.initModals($scope, controller, apiName)
-crud.init($scope, controller, apiName)
-
+    $scope.apiUrl = mainAsset.getUrl() + apiName;
+    crud.initModals($scope, controller, apiName);
+    crud.init($scope, controller, apiName);
+    
+    controller.doConvert = function(){
+        for(var i =0;i<controller.note.length;i++){
+            marked(controller.note[i].message,function(err,content){
+                $('#notif-'+i)[0].innerHTML = content ;            
+            })
+        }
+    }
+    
+    controller.getData = function(url,callback) {
+        if(url){
+          var crudGetUrl = url;
+        }else{
+          var crudGetUrl = scope.getUrl;
+        }
+        requestHelper.get(
+          crudGetUrl, scope,
+          function(response) {
+            if(callback)
+              {
+                callback(response);
+              }else{
+                scope.meta = response.data.meta;
+                if(response.data.total_price) scope.total_price = response.data.total_price;
+                else delete scope['total_price']
+                controller.note = response.data[apiName + 's'];
+                setTimeout(controller.doConvert,200);
+            }
+            console.log(response)  
+          },true);
+      };
+    controller.getData($scope.apiUrl);
+    
 });
